08b2fafdf7120f6725e4c33b946b2963844414b5
Speedwagon10 <105142258+Speedwagon10@users.noreply.github.com>
2024-07-20 14:40:17 +0300

    Change ColorBlock

--- src/components/ColorBlock/index.tsx
+++ src/components/ColorBlock/index.tsx
@@ -2,33 +2,33 @@ import { useState } from "react";
 import { FaCheckCircle } from "react-icons/fa";
 import { ColorBlockPropsType } from "../../types";
 
-
-const ColorBlock = ( props: ColorBlockPropsType ) => {
-    const [copied, setCopied]    = useState(false);
+const ColorBlock = (props: ColorBlockPropsType) => {
+    const [copied, setCopied] = useState(false);
 
     const iconDuration = 2000;
-    const divStyle     = {
+    const divStyle = {
         backgroundColor: props.color,
     };
 
     const handleClick = () => {
         navigator.clipboard.writeText(props.color);
-        setCopied( true );
-        setTimeout( () => {
-            setCopied( false );
-        }, iconDuration );
+        setCopied(true);
+        setTimeout(() => {
+            setCopied(false);
+        }, iconDuration);
     };
 
     return (
-        <div
-            onClick={handleClick}
-            style={divStyle}
-            className="color-block"
-        >
+        <div onClick={handleClick} style={divStyle} className="color-block">
             {copied ? (
-                <FaCheckCircle 
-                    className={`w-7 h-7 text-${props.mode === "dark" ? "white" : "black"}`} 
-                />
+                <>
+                    <p className="font-semibold">Copied </p>
+                    <FaCheckCircle
+                        className={`w-5 h-5 ml-1 text-${
+                            props.mode === "dark" ? "white" : "black"
+                        }`}
+                    />{" "}
+                </>
             ) : (
                 <h2>{props.color}</h2>
             )}
